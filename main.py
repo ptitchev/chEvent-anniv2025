2e874a978017da63c88a030a61bde2a0098fb8bc
ptitchev <jules.chevenet@gmail.com>
2025-05-19 01:18:45 +0200

    fix

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-from database.create import create_tables
-from database.init import init_db
+#from database.create import create_tables
+#from database.init import init_db
 from topics.runner import init_state, run_page, add_common_style, st_db_health
 
-reset = False
-update = False
+#eset = False
+#update = False
 
-if reset:
-    init_db()
-elif update:
-    create_tables()
+#if reset:
+    #init_db()
+#elif update:
+    #create_tables()
 
 if st_db_health():
     init_state()
